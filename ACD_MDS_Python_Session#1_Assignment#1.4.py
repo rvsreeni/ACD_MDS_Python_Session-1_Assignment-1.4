ceff68ded1bdab2d029e9437338f8012687ff488
rvsreeni <rvsreeni@gmail.com>
2017-11-09 08:35:09 +0530

    Add files via upload

---
+++ ACD_MDS_Python_Session#1_Assignment#1.4.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Prints Volume of sphere with diameter 12 cm
+"""
+
+dia = 12
+rad = dia / 2
+volsph = (4 * 22 * rad**3 ) / (3 * 7)
+print("Volume of sphere of diameter 12 cm is %4.2f cubic cm" % (volsph))
